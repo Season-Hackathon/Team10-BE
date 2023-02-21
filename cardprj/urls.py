a52b4fec9843a547123e63f0572ea8ec82f8d811
swimkim <81215099+swim-kim@users.noreply.github.com>
2023-02-22 04:52:50 +0900

    feat : 필드 수정

--- cardprj/urls.py
+++ cardprj/urls.py
@@ -24,6 +24,5 @@ from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path("accounts/",include('accounts.urls'))
 ] 
