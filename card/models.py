4a9d4a295a268f6375a119ddd4b5cfb20d034aee
jiinkyung <jinkyung010310@gmail.com>
2023-02-24 23:45:43 +0900

    fix : 모델 수정

--- card/models.py
+++ card/models.py
@@ -9,12 +9,13 @@ class Card(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.OneToOneField(CustomUser, null=True, blank=False, on_delete=models.CASCADE)
     classname = models.CharField(max_length=10,blank=False,null=True)
-    #userImage = models.ImageField(blank=True, null=True, upload_to="ProfileImage")
+    userImage = models.ImageField(blank=True, null=True, upload_to="ProfileImage")
 
     friends = models.ManyToManyField(CustomUser, blank=True, related_name='friends')
 
     def __str__(self):
         return f'{self.user.username}의 카드'
+    
 class Tag(models.Model):
     card = models.ForeignKey(Card, related_name='tags',on_delete=models.CASCADE)
     tagname = models.CharField(max_length=20,blank=False,default='')
